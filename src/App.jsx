f4e6cabb56367b6ef987eb5d5b9919d9f020e2b4
Fabian Cordoba <fabiansb98@gmail.com>
2022-11-25 10:11:49 -0600

    Preparación de preguntas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import questions from "./questions";
+import { useState, useEffect } from "react";
 function App() {
-  return <main className="app"></main>
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [isFinished, setIsFinished] = useState(false);
+  return <main className="app">
+    <div className="left-side">
+      <div className="number-question">
+        <span> Pregunta {currentQuestion + 1} de</span> {questions.length}
+      </div>
+      <div className="title-question">
+        ¿Pregunta?
+      </div>
+    </div>
+    <div className="right-side">
+      <button>Opcion 1</button>
+      <button>Opcion 2</button>
+      <button>Opcion 3</button>
+      <button>Opcion 4</button>
+    </div>
+  </main>
 }
-
-export default App
+export default App;
---
+++ src/questions.js
@@ -0,0 +1,48 @@
+const questions = [
+    {
+        question: "¿ Esta app fue desarrollada con ?",
+        options: [
+            { textAnswer: "React", isCorrect: true},
+            { textAnswer: "PHP", isCorrect: false},
+            { textAnswer: "Java", isCorrect: false},
+            { textAnswer: "C++", isCorrect: false},
+        ],
+    },
+    {
+        question: "¿ Nacionalidad del desarrollador ?",
+        options: [
+            { textAnswer: "Ruso", isCorrect: false},
+            { textAnswer: "Español", isCorrect: false},
+            { textAnswer: "Estadounidense", isCorrect: false},
+            { textAnswer: "Costarricense", isCorrect: true},
+        ],
+    },
+    {
+        question: "¿ Año en que se desarrollo la app ?",
+        options: [
+            { textAnswer: "2010", isCorrect: false},
+            { textAnswer: "2014", isCorrect: true},
+            { textAnswer: "2018", isCorrect: false},
+            { textAnswer: "2022", isCorrect: false},
+        ],
+    },
+    {
+        question: "¿ tiempo de desarrollo  ?",
+        options: [
+            { textAnswer: "meses", isCorrect: false},
+            { textAnswer: "años", isCorrect: false},
+            { textAnswer: "dias ", isCorrect: true},
+            { textAnswer: "semanas", isCorrect: false},
+        ],
+    },
+    {
+        question: "¿ Repositorio en  ?",
+        options: [
+            { textAnswer: "GitHub", isCorrect: true},
+            { textAnswer: "Facebook", isCorrect: false},
+            { textAnswer: "Youtube", isCorrect: false},
+            { textAnswer: "Instagram", isCorrect: false},
+        ],
+    },
+]
+export default questions;
